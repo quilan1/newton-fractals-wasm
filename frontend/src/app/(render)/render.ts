958bd5878ecffb850c903a18d0326bec7c63b40c
John Reynolds <quilan@gmail.com>
2023-11-30 11:32:10 -0500

    Changed color spread algorithm to maximize adjacency diffs

--- frontend/src/app/(render)/render.ts
+++ frontend/src/app/(render)/render.ts
@@ -43,49 +43,35 @@ const _drawRoots = (data: RenderStateData, context: CanvasRenderingContext2D) =>
     }
 }
 
-const angleDelta = (a: number, b: number): number => {
-    const delta = (a - b + 360) % 360;
-    const deltaNeg = -((360 - delta) % 360);
-    return (Math.abs(delta) < Math.abs(deltaNeg)) ? delta : deltaNeg;
-}
+export const spreadColors = (colors: OklchColor[]) => {
+    const angleEpsilon = 5;
 
-export const spreadColors = (_colors: OklchColor[]) => {
-    const colors = _colors.map(c => ({ h: c.h, c: c.c }));
-
-    for (const color of colors) { color.h += color.c; }
-    for (const color of colors) { color.h += 360; color.h %= 360; }
-    // for (const color of colors) { color.h += color.h / 360.0; }
+    // Shift them up by 5 deg, so that things near the 0 mark, but negative are counted as 0'ish
+    for (const color of colors) { color.h += 360 + angleEpsilon; color.h %= 360; }
     colors.sort((a, b) => a.h - b.h);
-    // console.log("Colors:", colors.map(c => ({ c: c.c.toFixed(2), h: c.h.toFixed(2) })));
 
-    for (let _ = 0; _ < 20; _++) {
-        let deltas: number[][] = [];
-        for (let i = 0; i < colors.length; ++i) {
-            const prevColor = (i + colors.length - 1) % colors.length;
-            const nextColor = (i + colors.length + 1) % colors.length;
-            const prevDelta = Math.abs(angleDelta(colors[prevColor].h, colors[i].h));
-            const nextDelta = Math.abs(angleDelta(colors[i].h, colors[nextColor].h));
-            const _deltas = [prevDelta, nextDelta].sort((a, b) => a - b);
-            deltas.push([i, ..._deltas, prevDelta, nextDelta]);
-        }
-        deltas.sort((a, b) => a[1] - b[1]);
-        if ((deltas[0][1] + 2) * colors.length > 360) break;
-        // console.log('deltas', deltas);
+    const initialAngle = colors[0].h - angleEpsilon;
+    const angleAmount = 360 / colors.length;
 
-        const lowestValue = deltas[0][1];
-        deltas = deltas.filter(d => d[1] === lowestValue);
-        deltas.sort((a, b) => a[2] - b[2]);
-
-        const lowestIndex = deltas[0][0];
-        const angleDeltas: [number, number][] = colors.map((_, j) => [j, Math.abs(angleDelta(colors[lowestIndex].h, colors[j].h))]);
-        angleDeltas.sort((a, b) => b[1] - a[1]);
-        // console.log('angleDeltas', angleDeltas);
-
-        const furthestIndex = angleDeltas[0][0];
-        const temp = colors[furthestIndex].h;
-        colors[furthestIndex].h = colors[lowestIndex].h;
-        colors[lowestIndex].h = temp;
+    // The goal of this algorithm is to spread out the colors as much as possible on the hue spectrum.
+    // We also want to maximize the distance between adjacent nodes, so this basically boils down to the
+    // following problem: Given an array [0..n-1], what permutation maximizes the closest-distance
+    // between adjacent elements, modulo-n? This algorithm permutes the array as needed to maximize
+    // the adjacent differences.
+    const n = colors.length;
+    let allIndices: number[];
+    if (!(colors.length & 1)) {
+        const indices = [...Array(colors.length / 2).keys()].map(i => i * (n / 2 + 1));
+        allIndices = [...indices, ...indices.map(v => (n * n + 2 * n - 4) / 4 - v)];
+    } else {
+        allIndices = [...Array(colors.length).keys()].map(i => i * (n - 1) / 2);
     }
+    // allIndices = [...Array(colors.length).keys()];
+    allIndices = allIndices.map(v => v % n);
+    colors.forEach((color, i) => { color.h = initialAngle + allIndices[i] * angleAmount });
+
+    // console.log("Colors:", colors.map(c => ({ c: c.c.toFixed(2), h: c.h.toFixed(2) })));
 
     return colors;
 }
+
