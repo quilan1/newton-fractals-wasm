685d4fe9c47dca3457ef1648ff7ec057f1dfa73a
John Reynolds <quilan@gmail.com>
2023-12-02 16:41:22 -0500

    Moved wasm-pack install into an optional command

--- crates/setup.sh
+++ crates/setup.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
 set -e
+set +x
 
 PKG=../frontend/src/pkg
 #OPT=--profiling
 OPT=--release
 
-cargo install wasm-pack
+CMD=$1
+
+if [ "$CMD" = "install-wasm" ]; then
+    curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+fi
+
 wasm-pack build -t bundler -d ../$PKG $OPT newton_wasm;
